636142c58b6af4bed4ea3ba572a3887bf959cc24
MarioSebastian544 <mariosebastian544@gmail.com>
2026-06-25 00:47:08 -0600

    initial commit

--- app/src/main/java/com/example/app_recetas/MainActivity.kt
+++ app/src/main/java/com/example/app_recetas/MainActivity.kt
@@ -4,13 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.app_recetas.ui.theme.APP_RecetasTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,30 +29,43 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+
+            val viewModel: RecipeViewModel =
+                viewModel()
             APP_RecetasTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(
+                    topBar = {
+                        topbar()
+                    }
+
+                ) { paddingValues ->
+                    AppNavigation(viewModel, modifier = Modifier.padding(paddingValues))
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun topbar(){
+    val topBarColor = Color(0xFF6A0032)
+    TopAppBar(
+        title = { Text(text = "IPN Catalogo de Videojuegos")},
+        navigationIcon = {
+            IconButton(onClick = { /* acción */ }) {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_launcher_foreground),
+                    contentDescription = "Logo",
+                    modifier = Modifier.size(50.dp)
+                )
+            }
+        },
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = topBarColor,
+            titleContentColor = Color.White
+        )
+
     )
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    APP_RecetasTheme {
-        Greeting("Android")
-    }
-}
